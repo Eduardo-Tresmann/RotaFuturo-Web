983e94b5e9c02e01fa99b563ea4903e3d230fc21
Eduardo-Tresmann <contatoeduardotresmann@gmail.com>
2025-08-28 19:28:23 -0400

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,52 +1,31 @@
-'use client';
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/context/ThemeContext";
 
-import '../styles/globals.css';
-import { Toaster } from 'sonner';
-import ClientProviders from '@/components/context/ClientProviders';
-import { ThemeProvider } from '@/components/context/ThemeContext';
-import { useEffect, useState } from 'react';
-import { SplashScreen } from '@/components/SplashScreen';
-import AuthSplashWrapper from '@/components/context/AuthSplashWrapper';
-
-import { usePathname } from 'next/navigation';
-import Head from 'next/head';
-
-
+export const metadata: Metadata = {
+  title: "RotaFuturo",
+  description: "Simulador de Carreira para o Ensino Médio!",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico" },
+      { url: "/favicon/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+      { url: "/favicon/favicon.svg", type: "image/svg+xml" },
+    ],
+    apple: [{ url: "/favicon/apple-touch-icon.png", sizes: "180x180" }],
+  },
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const pathname = usePathname();
-
+}) {
   return (
-    <>
     <html lang="pt-BR">
-      <Head>
-        <title>Nome da Página</title>
-        <meta name="description" content="Descrição da página" />
-      </Head>
-      <body className={`antialiased bg-zinc-50 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-50`}>
-          <ThemeProvider>
-            <ClientProviders>
-              <AuthSplashWrapper pathname={pathname}>
-                <div className="min-h-screen">
-                  <main>{children}</main>
-                  <Toaster
-                    position="top-right"
-                    richColors
-                    closeButton
-                    duration={4000}
-                    expand={true}
-                    className="z-50"
-                  />
-                </div>
-              </AuthSplashWrapper>
-            </ClientProviders>
-          </ThemeProvider>
+      <body className="antialiased bg-zinc-50 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-50">
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
-    </>
   );
 }
